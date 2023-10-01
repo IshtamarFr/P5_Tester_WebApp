e0e508b6940b259066d3d5587bc60e1e3f1d7739
IshtamarFr <ishta@ishtamar.fr>
2023-10-01 13:37:58 +0200

    jest OK for sessions

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,9 +2,20 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
+
+  let mockUser: SessionInformation = {
+    token: 'mockToken',
+    type: 'mockType',
+    id: 1,
+    username: 'mockUserName',
+    firstName: 'mockFirstName',
+    lastName: 'mockLastName',
+    admin: true,
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,5 +24,15 @@ describe('SessionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be logged in', () => {
+    service.logIn(mockUser);
+    expect(service.isLogged).toBeTruthy;
+  });
+
+  it('should be logged out', () => {
+    service.logOut();
+    expect(service.isLogged).not.toBeTruthy;
   });
 });
