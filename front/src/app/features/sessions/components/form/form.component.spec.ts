7be1df606eca6e295e45a9f8186d1714534ccfea
IshtamarFr <ishta@ishtamar.fr>
2023-10-01 14:55:00 +0200

    deprecated TestBed

--- front/src/app/features/sessions/components/form/form.component.spec.ts
+++ front/src/app/features/sessions/components/form/form.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {  ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -14,20 +14,21 @@ import { SessionService } from 'src/app/services/session.service';
 import { SessionApiService } from '../../services/session-api.service';
 
 import { FormComponent } from './form.component';
+import { Router } from '@angular/router';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let router: Router;
 
   const mockSessionService = {
     sessionInformation: {
-      admin: true
-    }
-  } 
+      admin: true,
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-
       imports: [
         RouterTestingModule,
         HttpClientModule,
@@ -35,25 +36,32 @@ describe('FormComponent', () => {
         MatIconModule,
         MatFormFieldModule,
         MatInputModule,
-        ReactiveFormsModule, 
+        ReactiveFormsModule,
         MatSnackBarModule,
         MatSelectModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
       ],
       providers: [
         { provide: SessionService, useValue: mockSessionService },
-        SessionApiService
+        SessionApiService,
       ],
-      declarations: [FormComponent]
-    })
-      .compileComponents();
+      declarations: [FormComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.get(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect non admin', () => {
+    mockSessionService.sessionInformation.admin = false;
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    component.ngOnInit();
+    expect(navigateSpy).toHaveBeenCalledWith(['/sessions']);
   });
 });
