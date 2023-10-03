7f74699d1530ff9c157170d69ef98b5994033bc7
IshtamarFr <ishta@ishtamar.fr>
2023-10-03 16:57:15 +0200

    First Unit Test OK : findUserById

---
+++ back/src/test/java/com/openclassrooms/starterjwt/UserServiceTest.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.starterjwt;
+
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    public void testFindUser() {
+        User mockUser=new User("test@test.com","Nom","Prenom","123456",true);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(mockUser));
+
+        User user=userService.findById(1L);
+        assertThat(user).isEqualTo(mockUser);
+        verify(userRepository,times(1)).findById(1L);
+    }
+}
