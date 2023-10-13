4a92c87e529a89cbd9d7e268a09d954d30bd0f51
IshtamarFr <ishta@ishtamar.fr>
2023-10-13 13:39:36 +0200

    started optimize imports on fly (test)

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerIT.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerIT.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
-public class AuthControllerTest {
+public class AuthControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
