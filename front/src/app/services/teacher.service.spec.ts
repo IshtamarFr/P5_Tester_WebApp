2fd7fb2db3d9a3ad7b2deaf5c10d14fb0b8e44a9
IshtamarFr <ishta@ishtamar.fr>
2023-10-18 10:11:01 +0200

    test get all teachers

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 import {
@@ -6,21 +7,47 @@ import {
 } from '@angular/common/http/testing';
 
 import { TeacherService } from './teacher.service';
+import { Teacher } from '../interfaces/teacher.interface';
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [TeacherService],
     });
     service = TestBed.inject(TeacherService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should get teacher by id', () => {});
+  it('should get all teachers', () => {
+    const response: Teacher[] = [
+      {
+        id: 42,
+        lastName: 'mockLastName',
+        firstName: 'mockFirstName',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    httpClient
+      .get<Teacher[]>(service['pathService'])
+      .subscribe((data) => expect(data).toEqual(response));
+
+    const req = httpTestingController.expectOne(service['pathService']);
+    expect(req.request.method).toEqual('GET');
+    req.flush(response);
+  });
 });
