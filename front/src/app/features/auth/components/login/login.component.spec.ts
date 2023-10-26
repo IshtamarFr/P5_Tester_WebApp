28cd04b672a3ee471a9260b1cb4caf6502644225
IshtamarFr <ishta@ishtamar.fr>
2023-10-26 14:01:37 +0200

    LoginComponent Tests OK

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -5,24 +5,43 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+} from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
 
 import { LoginComponent } from './login.component';
 import { SessionService } from '../../../../services/session.service';
 import { AuthService } from '../../services/auth.service';
+import { routes } from '../../../../app-routing.module';
+import { Router } from '@angular/router';
+import { SessionInformation } from '../../../../interfaces/sessionInformation.interface';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
+  let router: Router;
+
+  let mockSessionInformation: SessionInformation = {
+    token: 'azertyuiop',
+    type: 'mockType',
+    id: 42,
+    username: 'mockUser',
+    firstName: 'mockFN',
+    lastName: 'mockLN',
+    admin: false,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService, AuthService],
+      providers: [SessionService],
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(routes),
         BrowserAnimationsModule,
         HttpClientModule,
         MatCardModule,
@@ -30,14 +49,25 @@ describe('LoginComponent', () => {
         MatFormFieldModule,
         MatInputModule,
         ReactiveFormsModule,
+        NoopAnimationsModule,
       ],
     }).compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
+    authService = TestBed.inject(AuthService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should submit', () => {
+    let authServiceSpy = jest
+      .spyOn(authService, 'login')
+      .mockReturnValue(of(mockSessionInformation));
+    component.submit();
+    expect(authServiceSpy).toHaveBeenCalled();
   });
 });
