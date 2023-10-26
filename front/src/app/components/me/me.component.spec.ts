841d35a5a57c52b2623ed9c65bd0630c6dc5e3ad
IshtamarFr <ishta@ishtamar.fr>
2023-10-26 11:44:02 +0200

    started MeComponent tests

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -5,41 +5,71 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { SessionService } from 'src/app/services/session.service';
+import { SessionService } from '../../services/session.service';
 
 import { MeComponent } from './me.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { User } from '../../interfaces/user.interface';
+import { UserService } from '../../services/user.service';
+import { of } from 'rxjs';
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
+  let userService: UserService;
+  let router: Router;
+
+  const mockUser: User = {
+    id: 1,
+    email: 'user1@test.com',
+    lastName: 'mockLN',
+    firstName: 'mockFN',
+    admin: false,
+    password: 'abcdefg',
+    createdAt: new Date(),
+  };
 
   const mockSessionService = {
     sessionInformation: {
       admin: true,
-      id: 1
-    }
-  }
+      id: 1,
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
+      providers: [{ provide: SessionService, useValue: mockSessionService }],
       imports: [
         MatSnackBarModule,
         HttpClientModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
+        RouterTestingModule,
       ],
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
-    })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    userService = TestBed.inject(UserService);
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should init with user id', () => {
+    let userServiceSpy = jest
+      .spyOn(userService, 'getById')
+      .mockReturnValue(of(mockUser));
+
+    component.ngOnInit();
+    expect(component['user']?.firstName).toBe('mockFN');
+    expect(userServiceSpy).toHaveBeenCalledWith('1');
   });
 });
