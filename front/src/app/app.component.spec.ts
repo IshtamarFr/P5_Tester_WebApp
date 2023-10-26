1e3088c063aeb461326e9b21330f0cd4607022c1
IshtamarFr <ishta@ishtamar.fr>
2023-10-26 10:48:41 +0200

    AppComponent OK

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -5,25 +5,50 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
 
 import { AppComponent } from './app.component';
-
+import { routes } from './app-routing.module';
+import { SessionService } from './services/session.service';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let router: Router;
+
+  const mockSessionService = {
+    $isLogged(): Observable<boolean> {
+      return of(true);
+    },
+    logOut(): void {},
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(routes),
         HttpClientModule,
-        MatToolbarModule
+        MatToolbarModule,
       ],
-      declarations: [
-        AppComponent
-      ],
+      providers: [{ provide: SessionService, useValue: mockSessionService }],
+      declarations: [AppComponent],
     }).compileComponents();
+    const fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    router = TestBed.inject(Router);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should check login state', () => {
+    let state!: boolean;
+    app.$isLogged().subscribe((x) => (state = x));
+    expect(state).toBe(true);
+  });
+
+  it('should logOut', () => {
+    let navigateSpy = jest.spyOn(router, 'navigate');
+    app.logout();
+    expect(navigateSpy).toHaveBeenCalledWith(['']);
   });
 });
