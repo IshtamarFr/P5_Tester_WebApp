e8e9419e62bf26a8ed56eb4733361976d3f6425f
IshtamarFr <ishta@ishtamar.fr>
2023-12-07 11:41:30 +0100

    AuthControllerIT is perfect

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/it/AuthControllerIT.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/it/AuthControllerIT.java
@@ -1,7 +1,11 @@
 package com.openclassrooms.starterjwt.controllers.it;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import com.openclassrooms.starterjwt.payload.request.SignupRequest;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -9,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -22,21 +27,71 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AuthControllerIT {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
 
     final ObjectMapper mapper=new ObjectMapper();
 
+    final User testUser=User.builder()
+            .email("987654321@test.com")
+            .password(passwordEncoder.encode("Aa123456!"))
+            .lastName("MockLN")
+            .firstName("MockFN")
+            .build();
+
+    @AfterEach
+    void clean(){
+        try {
+            userRepository.deleteById(userRepository.findByEmail("987654321@test.com").get().getId());
+        } catch (Exception e) {
+            //ignore
+        }
+    }
+
+
     @Test
-    public void testLoginAdmin() throws Exception {
+    public void testLoginUserWorksWithCorrectCredential() throws Exception {
+        //Given
+        try {
+            userRepository.save(testUser);
+        } catch (Exception e) {
+            //ignore
+        }
+
         LoginRequest loginRequest=LoginRequest.builder()
-                .email("yoga@studio.com")
-                .password("test!1234")
+                .email("987654321@test.com")
+                .password("Aa123456!")
                 .build();
 
+        //When
         this.mockMvc.perform(post("/api/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(loginRequest)))
+
+        //Then
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("dmin")))
-                .andExpect(content().string(containsString("true")));
+                .andExpect(content().string(containsString("LN")))
+                .andExpect(content().string(containsString("false")));
+    }
+
+    @Test
+    public void testRegisterUserWorks() throws Exception {
+        //Given
+        SignupRequest signupRequest= SignupRequest.builder()
+                .email("987654321@test.com")
+                .password("Aa123456!")
+                .lastName("MockLN")
+                .firstName("MockFN")
+                .build();
+
+        //When
+        this.mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(signupRequest)))
+
+        //Then
+                .andExpect(status().isOk());
     }
 }
