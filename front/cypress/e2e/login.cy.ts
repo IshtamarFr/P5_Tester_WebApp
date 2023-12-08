62d87a70005da4f58244ee1994d196b33524bc6c
IshtamarFr <ishta@ishtamar.fr>
2023-12-08 18:49:38 +0100

    login.cy working (start)

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,7 +1,6 @@
 describe('Login spec', () => {
-  it('Login successfull', () => {
-    cy.visit('/login');
-
+  it('Login successfully with correct credentials should lead to sessions', () => {
+    //Given
     cy.intercept('POST', '/api/auth/login', {
       body: {
         id: 1,
@@ -20,11 +19,33 @@ describe('Login spec', () => {
       []
     ).as('session');
 
+    //When
+    cy.visit('/login');
     cy.get('input[formControlName=email]').type('yoga@studio.com');
     cy.get('input[formControlName=password]').type(
       `${'test!1234'}{enter}{enter}`
     );
 
+    //Then
     cy.url().should('include', '/sessions');
+    cy.get('p').should('not.exist');
+  });
+
+  it('Login fails with incorrect credentials', () => {
+    //Given
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 400,
+    });
+
+    //When
+    cy.visit('/login');
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type(
+      `${'test!1234'}{enter}{enter}`
+    );
+
+    //Then
+    cy.url().should('include', '/login');
+    cy.get('p').should('have.class', 'error');
   });
 });
